41c51d1115b2b51a7cc8849abed62c56df824ba3
Mehdi Ait Mouh <mehdi.aitmouh.dev@gmail.com>
2023-03-27 14:54:14 +0000

    function that produces output according to a format.

---
+++ _printf.c
@@ -0,0 +1,43 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _printf - produces output according to a format.
+ * @format: format output
+ * Return: length if success or -1 if Error
+ */
+int _printf(const char *format, ...)
+{
+	va_list ptr;
+	int i = 0, len = 0;
+
+	if (format == NULL)
+		return (-1);
+	va_start(ptr, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] != '%')
+		{
+			_putchar(format[i]), len++, i++;
+			continue;
+		}
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] != '\0')
+			{
+				len += print_char(format[i], ptr);
+			}
+			else
+			{
+				va_end(ptr);
+				return (-1);
+			}
+
+		}
+		i++;
+	}
+	va_end(ptr);
+	return (len);
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_string(char *);
+int print_char(char c, va_list ptr);
+
+#endif
